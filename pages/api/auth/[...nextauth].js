a390491c8e7cf504bcb3d4b75f8296a4c696c4d6
andreisaac <andredisaac@gmail.com>
2024-04-27 03:02:12 +0100

    Delete [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++
@@ -1,8 +0,0 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcryptjs";
-import User from '../../../models/user';
-import aOptions from './auth';
-
-export default NextAuth(aOptions);
