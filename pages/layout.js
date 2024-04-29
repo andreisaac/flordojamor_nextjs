fb4c16be3f92804c24cfb2fd43e2cb14a9cd22e1
andreisaac <andredisaac@gmail.com>
2024-04-29 17:21:16 +0100

    web vitals

--- pages/layout.js
+++ pages/layout.js
@@ -1,8 +1,6 @@
 import Navbar from '@/components/navbar'
 import Footer from '@/components/footer'
 import styles from './index.module.sass'
-import Head from 'next/head'
-import Script from 'next/script'
 import Image from 'next/image'
 import background from './images/background.jpg'
 
@@ -10,7 +8,6 @@ export default function RootLayout({ children }) {
   return (
     <>
       <div className={styles.body} lang="pt-PT">
-        <Script src="https://kit.fontawesome.com/5b691653ea.js" crossOrigin="anonymous"></Script>
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <Navbar/>
         {background ? 
