c4506bb2a5d4fa20297d7b3940d032fe58fc506b
andreisaac <andredisaac@gmail.com>
2024-04-29 02:21:12 +0100

    fix lang bug

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export const fetchCache = 'force-no-store';
 
 export const metadata = {
   title: 'Menu',
-  description: 'Nesta página pode consultar o menu do dia do Restaurante Flor do Jamor, em baixo tem os contatos telefónicos assim como a morada. Pratos do dia atualizados diáriamente por volta das 10h30.',
+  description: 'Nesta página pode consultar o menu do dia do Restaurante Flor do Jamor, em baixo tem os contatos telefónicos assim como a morada. Pratos do dia atualizados diáriamente por volta das 10h30',
 }
 
 const Menu = ({pratosDia, pratosCarne, pratosPeixe}) => {
--- pages/layout.js
+++ pages/layout.js
@@ -9,14 +9,13 @@ import background from './images/background.jpg'
 export default function RootLayout({ children }) {
   return (
     <>
-      <Head>
-        <html lang="pt-PT"/>
-      </Head>
-      <div className={styles.body}>
+      <div className={styles.body} lang="pt-PT">
         <Script src="https://kit.fontawesome.com/5b691653ea.js" crossOrigin="anonymous"></Script>
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <Navbar/>
-        <Image className={styles.background} src={background} alt="Picture of the background" sizes="100vw" priority={true}/>
+        {background ? 
+          <Image className={styles.background} src={background} alt="Picture of the background" sizes="100vw" priority={true}/>
+          : ""}
         <div>
           {children}
         </div>
