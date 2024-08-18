a5dfd856c24ce950d772c27010d7bf4eb9746e2a
weezac <andredisaac@gmail.com>
2024-08-18 23:51:37 +0100

    weelytical-react added

--- pages/layout.js
+++ pages/layout.js
@@ -4,6 +4,7 @@ import styles from './index.module.sass'
 import Image from 'next/image'
 import Head from 'next/head'
 import background from './images/background.jpg'
+import Weelytical from 'weelytical-react'
 export default function RootLayout({ children }) {
   return (
     <>
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
       </Head>
       <div className={styles.body} lang="pt-PT">
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        <Weelytical/>
         <Navbar/>
         {background ? 
           <Image className={styles.background} src={background} alt="Picture of the background" sizes="100vw" priority={true}/>
